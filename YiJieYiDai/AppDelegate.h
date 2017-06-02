0f75b6b40027bd28cb515d5e277c8c2e05c12fbc
lijunjie0801 <1025400619@qq.com>
2017-06-02 14:34:27 +0800

    Initial Commit

---
+++ YiJieYiDai/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  YiJieYiDai
+//
+//  Created by lijunjie on 2017/6/2.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ YiJieYiDai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YiJieYiDai
+//
+//  Created by lijunjie on 2017/6/2.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
